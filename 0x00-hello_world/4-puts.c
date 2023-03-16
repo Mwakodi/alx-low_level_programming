c1ebdf0cac3c87417de24ca5e4785c81ad7f3fa8
mwakodi <smwakodi20@gmail.com>
2023-03-16 15:05:51 -0700

    task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+
+
 /**
-*main - Entry
-*Return: Always 0
+* main - prints "programming is like building a multilingual puzzle.
+* Return: Always 0.
 */
+
 int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
+}
