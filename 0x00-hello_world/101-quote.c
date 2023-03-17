0b3db4cbd5d4a3fb24a78144b08461beda1f1ae9
mwakodi <smwakodi20@gmail.com>
2023-03-16 18:25:00 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -10,5 +11,5 @@
 int main(void)
 
 {
-	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n",59);
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 	return (1);
