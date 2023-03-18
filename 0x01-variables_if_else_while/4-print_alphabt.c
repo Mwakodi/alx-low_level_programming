a0bbbbd178147a04f2c932033cabf190f3218bc5
mwakodi <smwakodi20@gmail.com>
2023-03-18 11:28:16 -0700

    task 7

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,15 +12,16 @@
 
 int main(void)
 {
-char low, e, q;
-e = 'e';
-q = 'q';
-
-for (low = 'a'; low <= 'Z'; low++)
-{
-if (low != e && low != q)
-putchar(low);
-}
-putchar('\n');
-return (0);
+	char low, e, q;
+	e = 'e';
+	q = 'q';
+	
+	for (low = 'a'; low <= 'Z'; low++)
+	{
+	if (low != e && low != q)
+	putchar(low);
+	}
+	putchar('\n');
+	
+	return (0);
 }
