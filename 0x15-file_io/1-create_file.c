8482f01ba7f99164c4d1aa5ff36a6ed0de6dcadb
Mwakodi <smwakodi20@gmail.com>
2023-05-08 06:09:11 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,24 +9,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int fd, ret;
-mode_t mode = S_IRUSR | S_IWUSR;
-ssize_t len = 0;
+int fd, w, len = 0;
 
-if (filename)
+if (filename == NULL)
 return (-1);
 
-fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
-if (fd == -1)
-return (-1);
-
-if (text_content)
-while (text_content[len])
+if (text_content != NULL)
+{
+for (len = 0; text_content[len];)
 len++;
+}
 
-ret = write(fd, text_content, len);
-if (ret == -1)
-close(fd);
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+w = write(fd, text_content, len);
+
+if (fd == -1 || w == -1)
 return (-1);
 
 close(fd);
